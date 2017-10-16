2d5ff052430078b898a1f1c8a790c5a31bfe4a7e
Andy Lui <awlui2014@gmail.com>
2017-10-16 14:40:26 -0700

    test: adjust test check

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,13 +5,12 @@ const helpers = require('yeoman-test');
 
 describe('generator-wp-2-tsc:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ someAnswer: true });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['package.json']);
   });
 });
